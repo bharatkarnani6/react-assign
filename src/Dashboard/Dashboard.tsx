03f834e59595fdc5ef62c3b8f5b5525eee87b5b5
Bharat Karnani <bharat.karnani6@gmail.com>
2020-10-16 10:16:58 +0530

    Changes

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import '../App.css'
-import {Bar} from 'react-chartjs-2';
+import './Dashboard.css'
+import AccountIcon from '../Assets/img/Sanal.svg'
+import { Bar } from 'react-chartjs-2';
 
 const state = {
   labels: ['January', 'February', 'March',
-           'April', 'May','June','July','August','Septmber','October','November','December'],
+    'April', 'May', 'June', 'July', 'August', 'Septmber', 'October', 'November', 'December'],
   datasets: [
     {
       label: 'Isuues',
@@ -13,41 +14,181 @@ const state = {
       backgroundColor: 'rgba(30, 139, 195, 1)',
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 2,
-      data: [65, 59, 80, 81, 56,32,87,34,76,54,84,24]
+      data: [65, 59, 80, 81, 56, 32, 87, 34, 76, 54, 84, 24]
     },
     {
-        label: 'Today Issue',
-        fill: false,
-        lineTension: 1,
-        backgroundColor: 'rgba(255, 0, 0, 0.2)',
-        borderColor: 'rgba(0,0,0,1)',
-        borderWidth: 2,
-        data: [50, 49, 40, 41, 65,32,46,23,78,45,76,26]
-      }
+      label: 'Today Issue',
+      fill: false,
+      lineTension: 1,
+      backgroundColor: 'rgba(255, 0, 0, 0.2)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [50, 49, 40, 41, 65, 32, 46, 23, 78, 45, 76, 26]
+    }
   ]
 }
 
 export default class Dashboard extends React.Component {
   render() {
     return (
-      <div className="col-12">
-        <Bar
-          data={state}
-          width={10}
-          height={3}
-          options={{
-            title:{
-              display:true,
-              text:'Issues in 2020',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
+      <div>
+        <div className="col-12">
+          <Bar
+            data={state}
+            width={10}
+            height={3}
+            options={{
+              title: {
+                display: true,
+                text: 'Issues in 2020',
+                fontSize: 20
+              },
+              legend: {
+                display: true,
+                position: 'right'
+              }
+            }}
+          />
+        </div><br></br>
+        <div className="row infos">
+          <div className="col-md-4">
+            <div className="card" style={{ width: "25rem" }}>
+              <div className="card-header text-center">
+                High Priority</div>
+              <ul className="list-group list-group-flush">
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="col-7">
+                      <h6 className="card-title font-weight-bold">Lorem ipsum dolor sit</h6>
+                    </div>
+                    <div className="col-5">
+                      <small className="card-date">January 02,2019</small>
+                    </div>
+                  </div>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="col-7">
+                      <h6 className="card-title font-weight-bold">Lorem ipsum dolor sit</h6>
+                    </div>
+                    <div className="col-5">
+                      <small className="card-date">January 02,2019</small>
+                    </div>
+                  </div>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="col-7">
+                      <h6 className="card-title font-weight-bold">Lorem ipsum dolor sit</h6>
+                    </div>
+                    <div className="col-5">
+                      <small className="card-date">January 02,2019</small>
+                    </div>
+                  </div>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card" style={{ width: "25rem" }}>
+              <div className="card-header text-center">
+                Recently Updated Issue</div>
+              <ul className="list-group list-group-flush">
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="col-7">
+                      <h6 className="card-title font-weight-bold">Lorem ipsum dolor sit</h6>
+                    </div>
+                    <div className="col-5">
+                      <small className="card-date">January 02,2019</small>
+                    </div>
+                  </div>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="col-7">
+                      <h6 className="card-title font-weight-bold">Lorem ipsum dolor sit</h6>
+                    </div>
+                    <div className="col-5">
+                      <small className="card-date">January 02,2019</small>
+                    </div>
+                  </div>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="col-7">
+                      <h6 className="card-title font-weight-bold">Lorem ipsum dolor sit</h6>
+                    </div>
+                    <div className="col-5">
+                      <small className="card-date">January 02,2019</small>
+                    </div>
+                  </div>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card" style={{ width: "25rem" }}>
+              <div className="card-header text-center">
+                All Issues</div>
+              <ul className="list-group list-group-flush">
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="icon">
+                      <img src={AccountIcon} />
+                    </div>
+                    <div className="info">
+                      <small className="">Bharat</small><br></br>
+                      <small className="">UI/UXDesigner</small>
+                    </div>
+                  </div>
+                  <div className="progress">
+                    <div className="progress-bar bg-danger" role="progressbar" style={{width: "25%"}} aria-valuenow={25} aria-valuemin={0} aria-valuemax={100}></div>
+                  </div>
+                  <div className="row data">
+                    <div className="col">
+                      <p>High Priority Issue: 8</p>
+                    </div>
+                    <div className="col">
+                    <p className="text-right">Total Issue: 10</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="list-group-item card-body">
+                  <div className="row">
+                    <div className="icon">
+                      <img src={AccountIcon} />
+                    </div>
+                    <div className="info">
+                      <small className="">Bharat</small><br></br>
+                      <small className="">UI/UXDesigner</small>
+                    </div>
+                  </div>
+                  <div className="progress">
+                    <div className="progress-bar bg-warning" role="progressbar" style={{width: "65%"}} aria-valuenow={25} aria-valuemin={0} aria-valuemax={100}></div>
+                  </div>
+                  <div className="row data">
+                    <div className="col">
+                      <p>High Priority Issue: 8</p>
+                    </div>
+                    <div className="col">
+                    <p className="text-right">Total Issue: 10</p>
+                    </div>
+                  </div>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+
---
+++ src/Issues/IssueList.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './IssueList.css';
+import AccountIcon from '../Assets/img/Sanal.svg'
+
+class IssueList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            datas: []
+        }
+    }
+    componentDidMount() {
+        axios.get('http://localhost:8000/api/v1/issues/')
+            .then(res => {
+                this.setState({ datas: res.data })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+    render() {
+        const { datas } = this.state;
+        return (
+            <div className="issue">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="shadow-sm p-3 mb-5 bg-white rounded"><h2>To-Do</h2></div>
+                        {
+                            datas.slice(0,3).map(d =>{
+                                return d.status==="TODO" ?
+                                <div key={d.id} className="card" style={{ width: "25rem" }}>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <h6 className="card-id col-6">{d.short_id}</h6>
+                                            <p className="card-date col-6 text-right">{new Date(d.created_at).toDateString()}</p>
+                                        </div>
+                                        <h5 className="card-title">{d.title}</h5>
+                                        <p className="card-text">{d.description}</p>
+                                        <div className="row">
+                                    <div className="col-6">
+                                        <p>Assignee</p>
+                                        <div className="row">
+                                            <div className="icon">
+                                                <img src={AccountIcon} />
+                                            </div>
+                                            <div className="info">
+                                                <small className="">Bharat</small><br></br>
+                                                <small className="">UI/UXDesigner</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-6 text-right">
+                                        <p>Priority</p>
+                                        {
+                                            d.priority=="LOW"?<button type="button" style={{ width: "80%" }} className="btn btn-warning btn-sm">{d.priority}</button>
+                                            :d.priority=="SHOWSTOPPER"?<button type="button" style={{ width: "80%" }} className="btn btn-primary btn-sm">{d.priority}</button>
+                                            :d.priority=="HIGH"?<button type="button" style={{ width: "80%" }} className="btn btn-danger btn-sm">{d.priority}</button>
+                                            :d.priority=="VERY LOW"?<button type="button" style={{ width: "80%" }} className="btn btn-warning btn-sm">{d.priority}</button>
+                                            :null
+                                        }
+                                    
+                                    </div>
+                                </div>
+                                    </div>
+                                </div>:null
+                            })
+                        }
+                    </div>
+                    <div className="col-md-4">
+                        <div className="shadow-sm p-3 mb-5 bg-white rounded">
+                            <h2>In Progress 2</h2>
+                        </div>
+                        {
+                            datas.map((d,i) =>{
+                                return d.status==="DOING" && i<105 ?
+                                <div key={d.id} className="card" style={{ width: "25rem" }}>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <h6 className="card-id col-6">{d.short_id}</h6>
+                                            <p className="card-date col-6 text-right">{new Date(d.created_at).toDateString()}</p>
+                                        </div>
+                                        <h5 className="card-title">{d.title}</h5>
+                                        <p className="card-text">{d.description}</p>
+                                        <div className="row">
+                                    <div className="col-6">
+                                        <p>Assignee</p>
+                                        <div className="row">
+                                            <div className="icon">
+                                                <img src={AccountIcon} />
+                                            </div>
+                                            <div className="info">
+                                                <small className="">Bharat</small><br></br>
+                                                <small className="">UI/UXDesigner</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-6 text-right">
+                                        <p>Priority</p>
+                                        {
+                                            d.priority=="LOW"?<button type="button" style={{ width: "80%" }} className="btn btn-warning btn-sm">{d.priority}</button>
+                                            :d.priority=="SHOWSTOPPER"?<button type="button" style={{ width: "80%" }} className="btn btn-primary btn-sm">{d.priority}</button>
+                                            :d.priority=="HIGH"?<button type="button" style={{ width: "80%" }} className="btn btn-danger btn-sm">{d.priority}</button>
+                                            :d.priority=="VERY LOW"?<button type="button" style={{ width: "80%" }} className="btn btn-warning btn-sm">{d.priority}</button>
+                                            :null
+                                        }
+                                    
+                                    </div>
+                                </div>
+                                    </div>
+                                </div>:null
+                            })
+                        }
+                        
+                    </div>
+                    <div className="col-md-4">
+                        <div className="shadow-sm p-3 mb-5 bg-white rounded"><h2>Done</h2></div>
+                        {
+                            datas.map((d,i) =>{
+                                return d.status==="DONE" && i<208 ?
+                                <div key={d.id} className="card" style={{ width: "25rem" }}>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <h6 className="card-id col-6">{d.short_id}</h6>
+                                            <p className="card-date col-6 text-right">{new Date(d.created_at).toDateString()}</p>
+                                        </div>
+                                        <h5 className="card-title">{d.title}</h5>
+                                        <p className="card-text">{d.description}</p>
+                                        <div className="row">
+                                    <div className="col-6">
+                                        <p>Assignee</p>
+                                        <div className="row">
+                                            <div className="icon">
+                                                <img src={AccountIcon} />
+                                            </div>
+                                            <div className="info">
+                                                <small className="">Bharat</small><br></br>
+                                                <small className="">UI/UXDesigner</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-6 text-right">
+                                        <p>Priority</p>
+                                        {
+                                            d.priority=="LOW"?<button type="button" style={{ width: "80%" }} className="btn btn-warning btn-sm">{d.priority}</button>
+                                            :d.priority=="SHOWSTOPPER"?<button type="button" style={{ width: "80%" }} className="btn btn-primary btn-sm">{d.priority}</button>
+                                            :d.priority=="HIGH"?<button type="button" style={{ width: "80%" }} className="btn btn-danger btn-sm">{d.priority}</button>
+                                            :d.priority=="VERY LOW"?<button type="button" style={{ width: "80%" }} className="btn btn-warning btn-sm">{d.priority}</button>
+                                            :null
+                                        }
+                                    
+                                    </div>
+                                </div>
+                                    </div>
+                                </div>:null
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+export default IssueList;
